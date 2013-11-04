f999acf3f3314eb5ea74bdd7e42f62377bbec9fd
Cristiano <Cristiano@Cristiano-PC>
2013-11-04 14:30:47 -0200

    Ultima Atualizacao

--- src/java/Model/VistoriaVeiculo.java
+++ src/java/Model/VistoriaVeiculo.java
@@ -2,12 +2,10 @@ package Model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
